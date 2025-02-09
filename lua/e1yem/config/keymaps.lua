0922dfcaaffe391a1282f1cd8db6ca2fb2002f1f
Luis Munoz Diaz <elyem.games@gmail.com>
2025-02-09 14:55:16 -0600

    update: keymaps for inserting lines above/below current line

--- lua/e1yem/config/keymaps.lua
+++ lua/e1yem/config/keymaps.lua
@@ -37,8 +37,8 @@ keymap("v", "<M-k>", ":move '<-2<cr>gv=gv", { desc = "Move up" })
 keymap("v", "<M-j>", ":move '>+1<cr>gv=gv", { desc = "Move down" })
 
 -- Insert line above/below
-keymap("n", "lk", "O<Esc>", { desc = "Insert line above" })
-keymap("n", "lj", "o<Esc>", { desc = "Insert line below" })
+keymap("n", "<leader>gO", "O<Esc>", { desc = "Insert line above" })
+keymap("n", "<leader>go", "o<Esc>", { desc = "Insert line below" })
 
 -- Join line
 keymap("n", "J", "mzJ`z", { desc = "Join current with next line and keep cursor position" })
