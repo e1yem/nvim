cd7c412814be359f0eabae3c17322e2aef3ac796
Luis Munoz Diaz <elyem.games@gmail.com>
2025-02-09 14:26:56 -0600

    fix: wrong spec definition for treesitter

--- lua/e1yem/plugins/treesitter.lua
+++ lua/e1yem/plugins/treesitter.lua
@@ -1,16 +1,16 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    cmd = { "TSUpdateSync" },
     opts = {
-        ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
-
         auto_install = true,
-
         highlight = {
             enable = true,
-            additional_vim_regex_highlighting = false
-        }
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
     },
     config = function(_, opts)
-        require("nvim-treesitter").setup(opts)
+        require("nvim-treesitter.configs").setup(opts)
     end
 }
